9a66cb7d55f470baef30acce5c742b4af213d500
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2024-06-28 21:30:55 +0800

    FStep2.编写登陆页面，实现数据双向绑定

--- frontend/admin/.eslintrc.js
+++ frontend/admin/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 关闭名称校验
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'eslint-disable-next-line':'off'
 
   }
 }
---
+++ frontend/admin/src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+        <div class="loginBox">
+            <a-form-model :rules="rules" :model="formdata" class="loginForm">
+                <a-form-model-item prop="username">
+                    <a-input v-model="formdata.username" placeholder="请输入用户名">
+                        <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+                    </a-input>
+                </a-form-model-item>
+                <a-form-model-item prop="password">
+                    <a-input v-model="formdata.password" type="password" placeholder="请输入密码">
+                        <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+                    </a-input>
+                </a-form-model-item>
+                <a-form-model-item class="loginBtn">
+                    <a-button type="primary" style="margin: 20px;">登陆</a-button>
+                    <a-button >取消</a-button>
+                </a-form-model-item>
+            </a-form-model>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 4, max: 12, message: '用户名必须在4到12个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 20, message: '密码必须在6到20个字符之间', trigger: 'blur' }
+        ]
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.container{
+    height: 100%;
+    background-color:#4a90e2  ;
+    /* display: flex;
+    justify-content: center;
+    align-items: center; 兼容性不是很好，不建议用来最外层来居中*/
+
+}
+.loginBox{
+    width: 450px;
+    height: 300px;
+    background-color: #f5f5f5;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    border-radius: 9px;
+
+}
+.loginForm{
+    width: 100%;
+    position: absolute;
+    bottom: 10%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.loginBtn{
+    display: flex;
+    justify-content: center;
+}
+</style>
