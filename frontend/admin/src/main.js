6e87b113363e9b0905d0dd8d5cb0974f68779c9c
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2024-06-28 16:53:20 +0800

    FStep1.前端架构与样式引入

--- frontend/admin/src/main.js
+++ frontend/admin/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import './plugin/antui'
 
+axios.defaults.baseURL = 'http://localhost:3000/api/v1'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- frontend/admin/src/router/index.js
+++ frontend/admin/src/router/index.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '../views/Login.vue'
+import Admin from '../views/Admin.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+  }
 ]
 
 const router = new VueRouter({
