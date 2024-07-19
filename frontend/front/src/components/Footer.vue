3d0fa4a03831cba5d8d0b162d9988024815d1602
JJH529529 <126366914+JJH529529@users.noreply.github.com>
2024-07-19 11:48:50 +0800

    前端展示1--基本框架搭建，topbar&footer

---
+++ frontend/front/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer class="bg-grey-lighten-1">
+          <v-col class="text-center " cols="12">
+            {{ new Date().getFullYear() }} — <strong>JiangBlog</strong>
+          </v-col>
+      </v-footer>
+</template>
---
+++ frontend/front/src/components/TopBar.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <v-app-bar app color="primary" flat>
+        <!-- logo -->
+
+        <v-avatar size="40" color="white"></v-avatar>
+
+        <v-container class="py-0 fill-height">
+            <v-btn  text color="white">首页</v-btn>
+            <v-btn v-for="item in cateList" :key="item.id" text color="white">{{item.name}}</v-btn>
+        </v-container>
+        <v-spacer></v-spacer>
+    <v-responsive max-width color="white">
+        <v-text-field dense flat hide-details rounded dark solo-inverted></v-text-field>
+    </v-responsive>
+    </v-app-bar>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      cateList: []
+    }
+  },
+  created () {
+    this.GetCateList()
+  },
+  methods: {
+    // 获取分类
+    async GetCateList () {
+      const { data: res } = await this.$http.get('category')
+      this.cateList = res.data
+    }
+  }
+}
+</script>
